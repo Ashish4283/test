0048c044907c71a32696ca2eb40a8b2a4b65a447
Ashish B Jiwa <43324295+Ashish4283@users.noreply.github.com>
2024-06-30 22:57:31 -0700

    Update your_script.js

--- your_script.js
+++ your_script.js
@@ -16,18 +16,19 @@ loadScript("https://cdn.jsdelivr.net/npm/fast-json-stable-stringify@2.1.0/index.
       return;
     }
 
-    fetch('https://script.google.com/macros/s/AKfycbxCwjo0lAyl2FLgWyA8nSl7hD_GRp5_fwdZ_VgKRNg2i3zzpyAl0fBHRSEGeLcARHg2/exec?imageUrl=' + encodeURIComponent(imageUrl))
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          console.error("Error fetching Base64 image:", data.error);
-          document.getElementById('output').textContent = JSON.stringify({ error: data.error }, null, 2);
-        } else {
-          extractTextFromBase64Image(data.base64);
-        }
+    // Fetch the image as a Blob
+    fetch(imageUrl)
+      .then(response => response.blob())
+      .then(blob => {
+        var reader = new FileReader();
+        reader.onloadend = function () {
+          var base64Data = reader.result.split(',')[1]; // Extract base64 data without the prefix
+          extractTextFromBase64Image(base64Data);
+        };
+        reader.readAsDataURL(blob); // Convert Blob to base64
       })
       .catch(error => {
-        console.error("Error fetching data from Google Apps Script proxy:", error);
+        console.error("Error fetching image:", error);
         document.getElementById('output').textContent = JSON.stringify({ error: error.toString() });
       });
   }
